37c2dabb82f5dc614feabc39985103ea24a3bd04
Joshua Kennedy <josh@outthinkgroup.com>
2019-07-22 22:39:44 -0400

    initial commit of the gatsby-theme-remix demo site

---
+++ gatsby-config.js
@@ -0,0 +1,49 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    // Used for the site title and SEO
+    title: `Gatsby Theme Remix`,
+    // Used to provide alt text for your avatar
+    author: `Josh Kennedy`,
+    //used in the post footer
+    shortBio: `a web designer and developer.`,
+    // Used for SEO
+    description: `This is a remix of the theme Gatsby Theme Blog. Changed some styling, added some components, also made extra comments to make extending this theme easier`,
+    // Used for social links in the root footer
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/JoshKen08672181`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/joshatoutthink`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: "@joshkennedy00/gatsby-theme-blog-remix",
+      options: {
+        basePath: "/",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Theme Remix`,
+        short_name: `remix`,
+        start_url: `/`,
+        background_color: `#F86278`,
+        theme_color: `#F86278`,
+        display: `standalone`,
+        icon: `src/images/remix-logo.svg`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
